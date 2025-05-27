2487c47ea68e3f0f3111df313df4c5147f14b329
Kirill <khoruzhii.ka@gmail.com>
2025-05-27 23:19:29 +0200

    fix

--- init.sh
+++ init.sh
@@ -28,7 +28,7 @@ curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot
 echo "### Creating dummy certificate for $domains..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -36,11 +36,11 @@ docker-compose run --rm --entrypoint "\
 
 # Start nginx
 echo "### Starting nginx..."
-docker-compose up --force-recreate -d nginx
+docker compose up --force-recreate -d nginx
 
 # Delete dummy certificate
 echo "### Deleting dummy certificate for $domains..."
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -57,7 +57,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "\
+docker compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
@@ -68,6 +68,6 @@ docker-compose run --rm --entrypoint "\
 
 # Reload nginx
 echo "### Reloading nginx..."
-docker-compose exec nginx nginx -s reload
+docker compose exec nginx nginx -s reload
 
 echo "### Done! Your site should now be available at https://$domains"
